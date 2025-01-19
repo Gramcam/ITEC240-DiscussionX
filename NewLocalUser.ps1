bce4b45ccb437d640dc001d79a6b054555ca32bd
Gramcam <121652975+Gramcam@users.noreply.github.com>
2025-01-19 09:24:12 -0700

    Local user Script from teams
    Our logins would be SF.<Firstname> your password will be P@$$W0rd

---
+++ NewLocalUser.ps1
@@ -0,0 +1,9 @@
+﻿
+$pw = "P@$$W0rd" 
+
+$names = @('John', 'Graham', 'Brady', 'Andy', 'Nick')
+
+$pwSec = ConvertTo-SecureString -String $pw -AsPlainText -Force
+foreach($name in $names){
+New-LocalUser -WhatIf -name "SF.$name" -Password $pwSec 
+}
